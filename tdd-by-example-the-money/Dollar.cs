2edaa78f9b92202429760227df3965b6013c56c3
JamesWang <jame2408@gmail.com>
2021-02-14 16:24:25 +0800

    ch1: [refactor] initialize amount by constructor parameter

--- tdd-by-example-the-money/Dollar.cs
+++ tdd-by-example-the-money/Dollar.cs
@@ -2,8 +2,11 @@ namespace tdd_by_example_the_money
 {
     public class Dollar
     {
+        private readonly int _amount;
+
         public Dollar(int amount)
         {
+            _amount = amount;
         }
 
         public double Amount { get; set; }
