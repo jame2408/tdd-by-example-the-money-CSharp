37ef0b4fe7d4fc5dc952b2138c3e32f2cdf517fa
JamesWang <jame2408@gmail.com>
2021-02-14 16:39:02 +0800

    ch2: [experiment] try five.times(3) should be 15 by current API

--- tdd-by-example-the-money-test/MoneyTest.cs
+++ tdd-by-example-the-money-test/MoneyTest.cs
@@ -11,6 +11,8 @@ namespace tdd_by_example_the_money_test
             var five = new Dollar(5);
             five.Times(2);
             Assert.AreEqual(10, five.Amount);
+            five.Times(3);
+            Assert.AreEqual(15, five.Amount);
         }
     }
 }
