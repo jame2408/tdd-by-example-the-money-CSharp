43fe0dd277ad7b4938444a9a3ab6bc0179f3d86b
JamesWang <jame2408@gmail.com>
2021-02-14 16:17:35 +0800

    ch1: [fake it] hard-code amount is 10 to pass test

--- tdd-by-example-the-money/Dollar.cs
+++ tdd-by-example-the-money/Dollar.cs
@@ -4,14 +4,12 @@ namespace tdd_by_example_the_money
     {
         public Dollar(int amount)
         {
-            throw new System.NotImplementedException();
         }
 
-        public double Amount { get; set; }
+        public double Amount { get; set; } = 10;
 
         public void Times(int multiplier)
         {
-            throw new System.NotImplementedException();
         }
     }
 }
