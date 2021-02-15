79ad996e5d2a9e471cd632eee6d418fe6b6aa90d
JamesWang <jame2408@gmail.com>
2021-02-15 12:54:28 +0800

    ch5: [test] add France multiplication test case

--- tdd-by-example-the-money-test/MoneyTest.cs
+++ tdd-by-example-the-money-test/MoneyTest.cs
@@ -19,5 +19,13 @@ namespace tdd_by_example_the_money_test
             Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
         }
+
+        [Test]
+        public void franc_multiplication()
+        {
+            var five = new Franc(5);
+            Assert.AreEqual(new Franc(10), five.Times(2));
+            Assert.AreEqual(new Franc(15), five.Times(3));
+        }
     }
 }
