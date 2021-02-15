f0e67a6962efc6abed0729620c9ebdc5890f0a02
JamesWang <jame2408@gmail.com>
2021-02-15 13:44:04 +0800

    ch6: [refactor] declare variable "france" type from Franc to Money type

--- tdd-by-example-the-money/Franc.cs
+++ tdd-by-example-the-money/Franc.cs
@@ -14,7 +14,7 @@ namespace tdd_by_example_the_money
 
         public override bool Equals(object? obj)
         {
-            var franc = (Franc) obj;
+            Money franc = (Franc) obj;
             return this.Amount == franc.Amount;
         }
     }
