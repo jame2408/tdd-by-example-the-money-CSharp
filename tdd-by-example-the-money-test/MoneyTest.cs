83309c59ee9714174d19f717c4b18a64882d3c8d
JamesWang <jame2408@gmail.com>
2021-02-16 16:41:31 +0800

    ch12: [experiment] reduced should came from bank.reduce()

--- tdd-by-example-the-money-test/MoneyTest.cs
+++ tdd-by-example-the-money-test/MoneyTest.cs
@@ -42,6 +42,7 @@ namespace tdd_by_example_the_money_test
             var sum = Money.Dollar(5).Plus(Money.Dollar(5));
             Assert.AreEqual(Money.Dollar(10), sum);
             //opposite down, starting from assertion and backward to find(think) where it came from
+            Money reduced = bank.Reduce(sum, "USD");
             Assert.AreEqual(Money.Dollar(10), reduced);
         }
     }
