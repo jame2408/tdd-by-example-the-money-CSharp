f05930e5ed0483688e9bca2dbf57565628b18a44
JamesWang <jame2408@gmail.com>
2021-02-16 17:31:04 +0800

    ch13: [refactor] make Sum class implements Expression interface

--- tdd-by-example-the-money/Sum.cs
+++ tdd-by-example-the-money/Sum.cs
@@ -1,6 +1,6 @@
 ﻿namespace tdd_by_example_the_money
 {
-    public class Sum
+    public class Sum : IExpression
     {
         public Money Augend { get; set; }
         public Money Addend { get; set; }
