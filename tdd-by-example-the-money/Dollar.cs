0ff2ae19dc3c2c7249ed1ec5157674667c37d789
JamesWang <jame2408@gmail.com>
2021-02-16 13:21:07 +0800

    ch11: [refactor] safe delete Dollar class

--- tdd-by-example-the-money/Dollar.cs
+++
@@ -1,9 +0,0 @@
-namespace tdd_by_example_the_money
-{
-    public class Dollar : Money
-    {
-        public Dollar(int amount, string currency) : base(amount, currency)
-        {
-        }
-    }
-}
