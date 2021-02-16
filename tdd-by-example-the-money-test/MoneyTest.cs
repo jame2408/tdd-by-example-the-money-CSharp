377d3af3c5ea3f7133aaf9c1d807ecee886f8576
JamesWang <jame2408@gmail.com>
2021-02-16 23:20:52 +0800

    ch14: [feat] create a private class Pair in Bank class as HashTable key

--- tdd-by-example-the-money-test/MoneyTest.cs
+++ tdd-by-example-the-money-test/MoneyTest.cs
@@ -82,11 +82,5 @@ namespace tdd_by_example_the_money_test
             var result = bank.Reduce(Money.Franc(2), "USD");
             Assert.AreEqual(Money.Dollar(1), result);
         }
-
-        [Test]
-        public void array_equals()
-        {
-            Assert.AreSame(new object[] {"abc"}, new object[] {"abc"});
-        }
     }
 }
--- tdd-by-example-the-money/Bank.cs
+++ tdd-by-example-the-money/Bank.cs
@@ -16,4 +16,16 @@
             return (from.Equals("CHF") && to.Equals("USD")) ? 2 : 1;
         }
     }
+
+    internal class Pair
+    {
+        private readonly string _from;
+        private readonly string _to;
+
+        public Pair(string from, string to)
+        {
+            this._from = from;
+            this._to = to;
+        }
+    }
 }
