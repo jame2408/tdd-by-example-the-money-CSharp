03de90038c2b36d31bc1f9b3b03a64ab56dd3582
JamesWang <jame2408@gmail.com>
2021-02-16 17:30:03 +0800

    ch13: [refactor] create Sum's constructor

--- tdd-by-example-the-money/Sum.cs
+++ tdd-by-example-the-money/Sum.cs
@@ -4,5 +4,10 @@
     {
         public Money Augend { get; set; }
         public Money Addend { get; set; }
+
+        public Sum(Money augend, Money addend)
+        {
+            
+        }
     }
 }
